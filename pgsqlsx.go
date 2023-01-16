39e4fecdf872a941ee40c4addcd557e49a377eba
den10b <den10b@yandex.ru>
2023-01-16 18:02:02 +0300

    Структуры БД

--- pgsqlsx.go
+++ pgsqlsx.go
@@ -9,19 +9,6 @@ import (
 	"time"
 )
 
-var schema = `
-CREATE TABLE person (
-    first_name text,
-    last_name text,
-    email text
-);
-
-CREATE TABLE place (
-    country text,
-    city text NULL,
-    telcode integer
-)`
-
 const (
 	host     = "localhost"
 	port     = 5432
@@ -32,30 +19,58 @@ const (
 )
 
 type Orders struct {
-	Id      uuid.UUID `db:"id" json:"id"`
-	Numb    string    `db:"track_number" json:"track_number"`
-	Entry   string    `db:"entry" json:"entry"`
-	Locale  string    `db:"locale" json:"locale"`
-	InSig   string    `db:"internal_signature" json:"internal_signature"`
-	CustId  string    `db:"customer_id" json:"customer_id"`
-	DelServ string    `db:"delivery_service" json:"delivery_service"`
-	ShK     string    `db:"shardkey" json:"shardkey"`
-	SmId    int       `db:"sm_id" json:"sm_id"`
-	DateCr  time.Time `db:"date_created" json:"date_created"`
-	OofSha  string    `db:"oof_shard" json:"oof_shard"`
+	Id                uuid.UUID `db:"id" json:"id"`
+	TrackNumber       string    `db:"track_number" json:"track_number"`
+	Entry             string    `db:"entry" json:"entry"`
+	Locale            string    `db:"locale" json:"locale"`
+	InternalSignature string    `db:"internal_signature" json:"internal_signature"`
+	CustomerId        string    `db:"customer_id" json:"customer_id"`
+	DeliveryService   string    `db:"delivery_service" json:"delivery_service"`
+	Shardkey          string    `db:"shardkey" json:"shardkey"`
+	SmId              int       `db:"sm_id" json:"sm_id"`
+	DateCreated       time.Time `db:"date_created" json:"date_created"`
+	OofShard          string    `db:"oof_shard" json:"oof_shard"`
 }
 type Payments struct {
-	Transaction  uuid.UUID `db:"transaction" json:"id"`
-	OrderUid     string    `db:"order_uid" json:"track_number"`
-	RequestId    string    `db:"request_id" json:"entry"`
-	Currency     string    `db:"currency" json:"locale"`
-	Provider     string    `db:"provider" json:"internal_signature"`
-	Amount       string    `db:"amount" json:"customer_id"`
-	PaymentDT    string    `db:"payment_dt" json:"delivery_service"`
-	Bank         string    `db:"bank" json:"shardkey"`
-	DeliveryCost int       `db:"delivery_cost" json:"sm_id"`
-	GoodsTotal   time.Time `db:"goods_total" json:"date_created"`
-	CustomFee    string    `db:"custom_fee" json:"oof_shard"`
+	Transaction  uuid.UUID `db:"transaction" json:"transaction"`
+	OrderUid     uuid.UUID `db:"order_uid" json:"order_uid"`
+	RequestId    string    `db:"request_id" json:"request_id"`
+	Currency     string    `db:"currency" json:"currency"`
+	Provider     string    `db:"provider" json:"provider"`
+	Amount       int       `db:"amount" json:"amount"`
+	PaymentDT    int       `db:"payment_dt" json:"payment_dt"`
+	Bank         string    `db:"bank" json:"bank"`
+	DeliveryCost int       `db:"delivery_cost" json:"delivery_cost"`
+	GoodsTotal   int       `db:"goods_total" json:"goods_total"`
+	CustomFee    int       `db:"custom_fee" json:"custom_fee"`
+}
+
+type Items struct {
+	Uid         uuid.UUID `db:"uid" json:"uid"`
+	OrderUid    uuid.UUID `db:"order_uid" json:"order_uid"`
+	ChrtId      int       `db:"chrt_id" json:"chrt_id"`
+	TrackNumber string    `db:"track_number" json:"track_number"`
+	Price       int       `db:"price" json:"price"`
+	Rid         string    `db:"rid" json:"rid"`
+	Name        string    `db:"name" json:"name"`
+	Sale        int       `db:"sale" json:"sale"`
+	Size        string    `db:"size" json:"size"`
+	TotalPrice  int       `db:"total_price" json:"total_price"`
+	NmId        int       `db:"nm_id" json:"nm_id"`
+	Brand       string    `db:"brand" json:"brand"`
+	Status      int       `db:"status" json:"status"`
+}
+
+type Delivery struct {
+	Uid      uuid.UUID `db:"uid" json:"uid"`
+	OrderUid uuid.UUID `db:"order_uid" json:"order_uid"`
+	Name     string    `db:"name" json:"chrt_id"`
+	Phone    string    `db:"phone" json:"track_number"`
+	Zip      string    `db:"zip" json:"price"`
+	City     string    `db:"city" json:"rid"`
+	Address  string    `db:"address" json:"name"`
+	Region   string    `db:"region" json:"sale"`
+	Email    string    `db:"email" json:"size"`
 }
 
 func main() {
